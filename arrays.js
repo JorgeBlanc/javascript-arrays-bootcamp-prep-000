ef3072074a86bfca21a67c6a49549e0aba1fa977
George White <thegeorgewilliam@gmail.com>
2018-04-19 17:49:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  return array
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
 }
